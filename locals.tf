907c7cc1e708733ad50dc1abe882494a642f946b
shailajae <shailaja00145@gmail.com>
2025-02-19 17:01:55 +0530

    vpc-module

--- locals.tf
+++ locals.tf
@@ -1,4 +1,4 @@
-locals {
+locals{
     resource_name ="${var.project_name}-${var.environment}"
     az_names  = slice(data.aws_availability_zones.available.names, 0, 2)
     default_vpc_id = data.aws_vpc.default.id
